a032a9a1655213492aa778372c209eaeb2368044
SetScript <101998415+SetScript@users.noreply.github.com>
2024-06-17 18:39:45 +0300

    Delete target directory

--- target/debug/deps/flame_client-83ca71faa7bc6293.d
+++
@@ -1,5 +0,0 @@
-C:\Users\pc\Desktop\project\flame-client\target\debug\deps\libflame_client-83ca71faa7bc6293.rmeta: src\main.rs
-
-C:\Users\pc\Desktop\project\flame-client\target\debug\deps\flame_client-83ca71faa7bc6293.d: src\main.rs
-
-src\main.rs:
