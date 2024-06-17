ad62f555fed743bf147b39cef3acfaec590cf979
lecrovvv <weirdoo145@gmail.com>
2024-06-17 21:08:28 +0300

    Right project structure added.

--- target/debug/deps/flame_client-1e28becae00749fe.d
+++
@@ -1,5 +0,0 @@
-C:\Users\pc\Desktop\project\flame-client\target\debug\deps\libflame_client-1e28becae00749fe.rmeta: src\main.rs
-
-C:\Users\pc\Desktop\project\flame-client\target\debug\deps\flame_client-1e28becae00749fe.d: src\main.rs
-
-src\main.rs:
